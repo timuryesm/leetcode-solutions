543a687afaa53fe48071a42e760c61786605a110
Timur Yesmukhan <102758106+timuryesm@users.noreply.github.com>
2025-11-29 11:56:05 -0500

    Create 3512.py

---
+++ easy/3512.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def minOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        total = sum(nums)
+        return total % k
