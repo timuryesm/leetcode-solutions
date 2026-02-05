f562d172e6a7fecf29dbca2830ca6eded19fa174
Timur Yesmukhan <102758106+timuryesm@users.noreply.github.com>
2026-02-05 12:41:13 -0500

    Implement transformed array construction in 3379.py

---
+++ easy/3379.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def constructTransformedArray(self, nums):
+        n = len(nums)
+        result = [0] * n
+        
+        for i in range(n):
+            if nums[i] == 0:
+                result[i] = 0
+            else:
+                new_index = (i + nums[i]) % n
+                result[i] = nums[new_index]
+                
+        return result
