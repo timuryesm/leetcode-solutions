11838f141d7aaffb599bdb53d6aeab9a48c3a54f
Timur Yesmukhan <102758106+timuryesm@users.noreply.github.com>
2026-02-26 12:49:16 -0500

    Implement numSteps function to calculate steps

---
+++ medium/1404.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numSteps(self, s):
+        steps = 0
+        carry = 0
+        
+        for i in range(len(s) - 1, 0, -1):
+            bit = int(s[i])
+            
+            if bit + carry == 1:
+                steps += 2
+                carry = 1
+            else:
+                steps += 1
+                
+        return steps + carry
