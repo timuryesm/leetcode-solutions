504b8d0db23ba2052f9731d7455eb0fea7450ef6
Timur Yesmukhan <102758106+timuryesm@users.noreply.github.com>
2026-04-22 11:46:25 -0400

    Implement twoEditWords method in Solution class

---
+++ medium/2452.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoEditWords(self, queries, dictionary):
+        res = []
+        
+        for q in queries:
+            for d in dictionary:
+                diff = 0
+                
+                for i in range(len(q)):
+                    if q[i] != d[i]:
+                        diff += 1
+                        if diff > 2:
+                            break
+                
+                if diff <= 2:
+                    res.append(q)
+                    break
+        
+        return res
