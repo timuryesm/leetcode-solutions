8e62f478542f351e2d317394e960483444dde0bd
Timur Yesmukhan <102758106+timuryesm@users.noreply.github.com>
2026-05-10 15:28:24 -0400

    Add maximumJumps method to Solution class
    Implement maximumJumps method to calculate maximum jumps within target difference.

---
+++ medium/2770.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maximumJumps(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        n = len(nums)
+        
+        # dp[i] = maximum jumps needed to reach index i
+        dp = [-1] * n
+        dp[0] = 0
+        
+        for i in range(n):
+            if dp[i] == -1:
+                continue
+                
+            for j in range(i + 1, n):
+                if abs(nums[j] - nums[i]) <= target:
+                    dp[j] = max(dp[j], dp[i] + 1)
+        
+        return dp[-1]
